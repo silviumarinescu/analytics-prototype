3493308365e4cf20d7cc7e38c10bc7ee9c3cb304
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-06-22 14:00:32 +0300

    added date

--- scripts/events/create-event.js
+++ scripts/events/create-event.js
@@ -1,5 +1,6 @@
 import db from '../../scripts/lib/db/index.js'
 import uuid from '../lib/utils/uuid.js'
+import moment from 'https://unpkg.com/moment@2.27.0/dist/moment.js'
 
 export const createEvent = (scope, name, version, payload, stream = uuid()) =>
   new Promise((accept, reject) => {
@@ -10,6 +11,7 @@ export const createEvent = (scope, name, version, payload, stream = uuid()) =>
         version,
         stream,
         payload,
+        date: moment().format('x'),
       })
       .then(() => accept(stream))
   })
